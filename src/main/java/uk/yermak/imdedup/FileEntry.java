6a28ac73b2f144651d6d82349ef2fa07716ac43b
yermak <yermak@gmail.com>
2016-10-21 00:42:29 +0100

    meta data

--- src/main/java/uk/yermak/imdedup/FileEntry.java
+++ src/main/java/uk/yermak/imdedup/FileEntry.java
@@ -2,10 +2,17 @@ package uk.yermak.imdedup;
 
 
 import org.apache.commons.io.FileUtils;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.zip.CRC32;
 import java.util.zip.Checksum;
@@ -69,6 +76,9 @@ public class FileEntry {
     }
 
     public void load() {
+//        readMetadata();
+
+
         if (crc32 == 0) {
             try {
                 Checksum checksum = new CRC32();
@@ -83,6 +93,34 @@ public class FileEntry {
         }
     }
 
+    private void readMetadata() {
+        try {
+            ImageInputStream iis = ImageIO.createImageInputStream(file);
+            Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+
+            if (readers.hasNext()) {
+
+                // pick the first available ImageReader
+                ImageReader reader = readers.next();
+
+                // attach source to the reader
+                reader.setInput(iis, true);
+
+                // read metadata of first image
+                IIOMetadata metadata = reader.getImageMetadata(0);
+
+                String[] names = metadata.getMetadataFormatNames();
+                int length = names.length;
+                for (int i = 0; i < length; i++) {
+                    System.out.println( "Format name: " + names[ i ] );
+                    displayMetadata(metadata.getAsTree(names[i]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public long getLength() {
         return file.length();
     }
@@ -93,5 +131,51 @@ public class FileEntry {
 
     public String getFileName() {
         return file.getName();
+    }
+
+
+    void displayMetadata(Node root) {
+        displayMetadata(root, 0);
+    }
+
+    void indent(int level) {
+        for (int i = 0; i < level; i++)
+            System.out.print("    ");
+    }
+
+    void displayMetadata(Node node, int level) {
+        // print open tag of element
+        indent(level);
+        System.out.print("<" + node.getNodeName());
+        NamedNodeMap map = node.getAttributes();
+        if (map != null) {
+
+            // print attribute values
+            int length = map.getLength();
+            for (int i = 0; i < length; i++) {
+                Node attr = map.item(i);
+                System.out.print(" " + attr.getNodeName() +
+                        "=\"" + attr.getNodeValue() + "\"");
+            }
+        }
+
+        Node child = node.getFirstChild();
+        if (child == null) {
+            // no children, so close element and return
+            System.out.println("/>");
+            return;
+        }
+
+        // children, so close current tag
+        System.out.println(">");
+        while (child != null) {
+            // print children recursively
+            displayMetadata(child, level + 1);
+            child = child.getNextSibling();
+        }
+
+        // print close tag of element
+        indent(level);
+        System.out.println("</" + node.getNodeName() + ">");
     }
 }
